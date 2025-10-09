727cafd1f15ce087a8444771348ff73f12b4b58d
Renaud Douze <renaud.douze@gmail.com>
2025-10-09 11:05:28 +0200

    Add individual route for labels

--- tests/src/Functional/Labels/LabelsTest.php
+++ tests/src/Functional/Labels/LabelsTest.php
@@ -8,6 +8,7 @@ use App\Controller\LabelsController;
 use App\Tests\Functional\Trait\ClientRequestTrait;
 use App\Tests\Functional\Trait\JsonResponseTrait;
 use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -19,7 +20,8 @@ class LabelsTest extends WebTestCase
     use ClientRequestTrait;
     use JsonResponseTrait;
 
-    public function testGetAll(): void
+    #[DataProvider('providerGetLabels')]
+    public function testGets(string $route, string $filename): void
     {
         $client = static::createClient();
 
@@ -27,11 +29,59 @@ class LabelsTest extends WebTestCase
             $client,
             'admin',
             'GET',
-            '/labels/all',
+            "/labels/{$route}",
         );
 
         $this->assertResponseIsSuccessful();
 
-        $this->assertResponseContent($client, 'Labels/all.json');
+        $this->assertResponseContent($client, "Labels/{$filename}.json");
+    }
+
+    /**
+     * @return array<string, array{
+     *     route: string,
+     *     filename: string
+     * }>
+     */
+    public static function providerGetLabels(): array
+    {
+        return [
+            'all' => [
+                'route' => 'all',
+                'filename' => 'all',
+            ],
+            'catch_states' => [
+                'route' => 'catch_states',
+                'filename' => 'catch_states',
+            ],
+            'types' => [
+                'route' => 'types',
+                'filename' => 'types',
+            ],
+            'forms/category' => [
+                'route' => 'forms/category',
+                'filename' => 'forms_category',
+            ],
+            'forms/regional' => [
+                'route' => 'forms/regional',
+                'filename' => 'forms_regional',
+            ],
+            'forms/special' => [
+                'route' => 'forms/special',
+                'filename' => 'forms_special',
+            ],
+            'forms/variant' => [
+                'route' => 'forms/variant',
+                'filename' => 'forms_variant',
+            ],
+            'game_bundles' => [
+                'route' => 'game_bundles',
+                'filename' => 'game_bundles',
+            ],
+            'collections' => [
+                'route' => 'collections',
+                'filename' => 'collections',
+            ],
+        ];
     }
 }
