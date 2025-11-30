8cbf341685beeb21c81ef12c2a226fa5bada2eba
Renaud Douze <renaud.douze@gmail.com>
2025-11-30 12:12:31 +0100

    Setup a editorconfig linter and fixes files

--- checkfiles.sh
+++ checkfiles.sh
@@ -4,12 +4,11 @@
 fichier_liste="liste_fichiers.txt"
 
 # Lire chaque ligne du fichier
-while IFS= read -r fichier
-do
-  # Vérifier si le fichier existe
-  if [ -f "$fichier" ]; then
-    echo "$fichier existe"
-  else
-    echo "$fichier existe pas"
-  fi
-done < "$fichier_liste"
+while IFS= read -r fichier; do
+	# Vérifier si le fichier existe
+	if [ -f "$fichier" ]; then
+		echo "$fichier existe"
+	else
+		echo "$fichier existe pas"
+	fi
+done <"$fichier_liste"
