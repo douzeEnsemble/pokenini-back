6e0dac5fc553140177ccf89291a64d2be035e75a
Renaud Douze <renaud.douze@gmail.com>
2026-04-05 11:19:39 +0200

    refactor: migrate Route imports from Annotation to Attribute namespace across all controllers

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -8,7 +8,7 @@ use App\Service\Api\GetActionLogsService;
 use App\Service\Api\GetReportsService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Serializer\SerializerInterface;
 
 #[Route('/istration')]
--- src/Controller/AlbumDexController.php
+++ src/Controller/AlbumDexController.php
@@ -10,7 +10,7 @@ use App\Service\Api\GetDexService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/album')]
 class AlbumDexController extends AbstractController
