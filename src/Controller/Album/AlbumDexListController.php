07851754da1e7255e4cba3d7233dbe85f85d62c6
Renaud Douze <renaud.douze@gmail.com>
2026-06-28 21:52:30 +0200

    Fix mandatory auth for listing dex

--- src/Controller/Album/AlbumDexListController.php
+++ src/Controller/Album/AlbumDexListController.php
@@ -9,6 +9,7 @@ use App\Security\UserTokenService;
 use App\Service\Api\GetDexListApiService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,16 +24,21 @@ final class AlbumDexListController extends AbstractController
     public function index(
         GetDexListApiService $getDexListService,
         UserTokenService $userTokenService,
+        Request $request,
     ): JsonResponse {
-        try {
-            $connectedUserId = $userTokenService->getLoggedUserToken();
-        } catch (NoLoggedUserException $e) {
-            return new JsonResponse([], Response::HTTP_UNAUTHORIZED);
+        $trainerId = $request->query->getAlnum('trainer_id', '');
+
+        if ('' === $trainerId) {
+            try {
+                $trainerId = $userTokenService->getLoggedUserToken();
+            } catch (NoLoggedUserException) {
+                return new JsonResponse([], Response::HTTP_UNAUTHORIZED);
+            }
         }
 
         $dex = $this->isGranted('ROLE_ADMIN')
-            ? $getDexListService->getWithUnreleasedAndPremium($connectedUserId)
-            : $getDexListService->getWithPremium($connectedUserId);
+            ? $getDexListService->getWithUnreleasedAndPremium($trainerId)
+            : $getDexListService->getWithPremium($trainerId);
 
         return new JsonResponse($dex, Response::HTTP_OK);
     }
