210ce7f68e699b8b0ded03dbc509e8562319e4a0
Renaud Douze <renaud.douze@gmail.com>
2026-08-09 10:04:36 +0200

    test: migrate Integration/Album tests to #[Test] attribute

--- tests/src/Integration/Album/AlbumDexListTest.php
+++ tests/src/Integration/Album/AlbumDexListTest.php
@@ -9,6 +9,7 @@ use App\Tests\Integration\Trait\ClientRequestTrait;
 use App\Tests\Integration\Trait\JsonResponseTrait;
 use PHPUnit\Framework\Attributes\CoversClass;
 use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\Attributes\Test;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 /**
@@ -21,7 +22,8 @@ final class AlbumDexListTest extends WebTestCase
     use ClientRequestTrait;
     use JsonResponseTrait;
 
-    public function testDexAsTrainer(): void
+    #[Test]
+    public function dexAsTrainer(): void
     {
         $client = self::createClient();
 
@@ -37,7 +39,8 @@ final class AlbumDexListTest extends WebTestCase
         $this->assertResponseContent($client, 'AlbumDexList/trainer.json');
     }
 
-    public function testDexAsCollector(): void
+    #[Test]
+    public function dexAsCollector(): void
     {
         $client = self::createClient();
 
@@ -53,7 +56,8 @@ final class AlbumDexListTest extends WebTestCase
         $this->assertResponseContent($client, 'AlbumDexList/collector.json');
     }
 
-    public function testDexAdmin(): void
+    #[Test]
+    public function dexAdmin(): void
     {
         $client = self::createClient();
 
@@ -69,7 +73,8 @@ final class AlbumDexListTest extends WebTestCase
         $this->assertResponseContent($client, 'AlbumDexList/admin.json');
     }
 
-    public function testDexPublicWithTrainerId(): void
+    #[Test]
+    public function dexPublicWithTrainerId(): void
     {
         $client = self::createClient();
 
@@ -84,7 +89,8 @@ final class AlbumDexListTest extends WebTestCase
         $this->assertResponseContent($client, 'AlbumDexList/trainer.json');
     }
 
-    public function testDexPublicWithoutTrainerId(): void
+    #[Test]
+    public function dexPublicWithoutTrainerId(): void
     {
         $client = self::createClient();
 
